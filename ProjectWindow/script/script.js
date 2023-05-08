f10668431aebe4329aeabea0997619814ea8dc3b
Tim2313 <tkisuk@gmail.com>
2023-05-09 01:09:49 +0300

    [ProjectWindow] create two buttons and quetion

--- ProjectWindow/script/script.js
+++ ProjectWindow/script/script.js
@@ -1,2 +1,17 @@
-
+const newspaperSpinning = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+  ];
+  
+  const newspaperTiming = {
+    duration: 2000,
+    iterations: 1,
+  };
+  
+  const newspaper = document.querySelector(".question");
+  
+  newspaper.addEventListener("click", () => {
+    newspaper.animate(newspaperSpinning, newspaperTiming);
+  });
+  
 
