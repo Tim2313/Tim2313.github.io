2c378dad6135a8a423f3603041a90d6f1562d4a1
Tim2313 <tkisuk@gmail.com>
2023-05-20 22:38:17 +0300

    [ProjectNotes] Create new index.html and script.js

--- NotesProject/script/script.js
+++ NotesProject/script/script.js
@@ -1,3 +1,30 @@
-document.getElementById("button_text").addEventListener('click', function () {
+console.log('Hello Worlda');
+// What is the difference between 'var', 'let' and 'const';
+// https://learn.javascript.ru/variables 
+// https://learn.javascript.ru/var
+// https://learn.javascript.ru/let-const
+
+function addNoteToList(event) {
+    event.preventDefault();
+    const noteInput = document.getElementById('noteInput').value;
+    const notesList = document.getElementById('notesList');
     
-});
+    const newListItem = document.createElement('li');
+    newListItem.innerHTML = noteInput;
+    notesList.appendChild(newListItem);
+}
+
+function deleteLastNote(){
+    const notesList = document.getElementById('notesList');
+    const length = notesList.childNodes.length;
+    if (length > 1) {
+        notesList.removeChild(notesList.lastChild);
+    } 
+}
+
+const noteForm = document.getElementById('noteForm');
+noteForm.addEventListener('submit', addNoteToList);
+
+const deleteLastNoteButton = document.getElementById('deleteTaskFromListButton');
+deleteLastNoteButton.addEventListener('click', deleteLastNote)
+
