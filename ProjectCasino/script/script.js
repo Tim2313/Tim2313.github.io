5267903adfe44b96c4fc79b2bd1b57fb76b78584
Tim2313 <tkisuk@gmail.com>
2023-05-31 01:25:17 +0300

    [ProjectCasino] Implement slots

---
+++ ProjectCasino/script/script.js
@@ -0,0 +1,46 @@
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 2) + 1;
+}
+
+function spin() {
+    const item1 = getElement('item1');
+    const item2 = getElement('item2');
+    const item3 = getElement('item3');
+
+    const num1 = getRandomNumber();
+    const num2 = getRandomNumber();
+    const num3 = getRandomNumber();
+
+    item1.innerHTML = `${num1}`;
+    item2.innerHTML = `${num2}`;
+    item3.innerHTML = `${num3}`;
+
+    if (num1 == num2 && num1 == num3) {
+        playWinSound();
+        showWinMassage();
+    } else {
+        hideMessage();
+    }
+}
+
+function playWinSound() {
+    const winSound = document.getElementById('winSound');
+    winSound.play();
+}
+
+function showWinMassage() {
+    const winMessage = document.getElementById('winMessage');
+    winMessage.style.display = 'block';
+    winMessage.classList.add('animated', 'pulse');
+}
+
+function hideMessage() {
+    const message = document.getElementById('winMessage');
+    message.style.display = 'none';
+}
+
+
